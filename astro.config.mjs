8e2e8a01e6ab6671c0ce9c4423c927aaae593795
Mathis S.M. <contact@minarox.fr>
2025-02-07 02:00:14 +0100

    build: Add astro-meta-tags dev tool

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import metaTags from 'astro-meta-tags';
 import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://racecast.minarox.fr',
-    integrations: [sitemap(), compressor()]
+    integrations: [sitemap(), metaTags(), compressor()]
 });
