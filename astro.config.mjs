9d19885d3d5afaa7c198d8fd481306b370ca0932
Mathis S.M. <contact@minarox.fr>
2025-02-07 01:44:30 +0100

    build: Add astro-compressor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import compressor from 'astro-compressor';
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://racecast.minarox.fr',
-    integrations: [sitemap()]
+    integrations: [sitemap(), compressor()]
 });
